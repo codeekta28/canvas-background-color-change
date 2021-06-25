1a5beec489b1125d061ee61ec624569b9f8e4840
Ekta <ektacode28@gmail.com>
2021-06-25 15:35:12 +0530

    add text to canvas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("This is index.js file");
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+context.font = "10px Arial";
+context.fillText("My Background color will change when button below is clicked", 10,70);
 document.querySelector(".btn").addEventListener("click",(e)=>{
- let canvas = document.querySelector("canvas");
  let colorChange = Math.floor(Math.random()*16777215).toString(16);
  canvas.style.backgroundColor = `#${colorChange}`;
 })
